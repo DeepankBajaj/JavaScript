0a1a36a7b7be7070bd01cc71919cae201db45386
DeepankBajaj <75161127+DeepankBajaj@users.noreply.github.com>
2024-03-05 19:48:05 +0000

    stack ,heap memory

--- 01_basics/05_datatype_summary.js
+++ 01_basics/05_datatype_summary.js
@@ -1,4 +1,4 @@
-//  Primitive
+//  Primitive- call by value- it gives copy of the original variable
 
 //  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
 
@@ -18,7 +18,7 @@ console.log(id === anotherId);
 
 
 
-// Reference (Non primitive)
+// Reference (Non primitive)- it gives refernce of original value and any changes occur to original value
 
 // Array, Objects, Functions
 
@@ -28,11 +28,11 @@ let myObj = {
     age: 22,
 }
 
-const myFunction = function(){
+/*const myFunction = function(){
     console.log("Hello world");
 }
 
-console.log(typeof anotherId);
+// console.log(typeof anotherId);
 
 /* Primitive datatypes:
 String - string
@@ -48,3 +48,28 @@ Function - function(object)
 Object - object */
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+/* +++++++++++++++++++++++++++++
+ Stack and Heap memory
+
+ Stack memory(for all primitive data types)- copy of the original variable
+ Heap(Non-primitive)- refernce of original
+*/
+let myName= "Deepank Bajaj"
+let yourName = myName
+yourName = "MS Dhoni"
+console.log(myName);
+console.log(yourName);
+
+let employee1 ={
+    eId: 1234,
+    eName:"Deepank",
+    eNumber:99999
+}
+console.log(employee1);
+
+let employee2 = employee1
+employee2.eId =9876
+
+console.log(employee1)
+console.log(employee2)
